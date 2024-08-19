50fd8d3a4a0b6d835c403ec3f1c5e2fe456599c5
sambutracy <tracysambut@gmail.com>
2024-08-19 19:53:07 +0300

    add task2

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,23 +1,27 @@
 import $ from 'jquery';
-import debounce from 'lodash/debounce';
-import '../css/main.css';
+import _ from 'lodash';
+import '../css/main.css'; // Import CSS file
 
-// Create and append elements
-$('body').prepend('<div id="logo"></div>');
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button id="start-btn">Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
-
-// Initialize counter
-let count = 0;
-
-// Function to update the counter
+// Function to update counter
 function updateCounter() {
-    count++;
+  let count = 0;
+  return function() {
+    count += 1;
     $('#count').text(`${count} clicks on the button`);
+  };
 }
 
-// Bind debounce function to button click event
-$('#start-btn').on('click', debounce(updateCounter, 300));
+const debouncedUpdateCounter = _.debounce(updateCounter(), 300);
+
+$(document).ready(function() {
+  $('body').prepend('<div id="logo"></div>'); // Add logo element
+  $('#logo').css('background-image', 'url(../assets/holberton-logo.jpg)');
+
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button id="startButton">Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  $('#startButton').on('click', debouncedUpdateCounter);
+});
--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,31 +1,34 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './js/dashboard_main.js', // Single entry point
-  output: {
-    filename: 'bundle.js', // Single output file
-    path: path.resolve(__dirname, 'public'),
-    clean: true, // Clean the output directory before emit
-  },
   mode: 'production',
+  entry: './js/dashboard_main.js',
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[hash][ext][query]',
-        },
+        test: /\.(png|jpe?g|gif)$/i,
+        use: ['file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+                bypassOnDebug: true,
+                disable: true,
+            },
+          },
+        ],
       },
-    ],
-  },
-  stats: 'verbose',
-  performance: {
-    maxAssetSize: 250000, // 250KB
-    maxEntrypointSize: 250000,
-  },
+    ]
+  }
 };
