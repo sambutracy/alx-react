fb6aa72d3a1a95bc85f58525995d6449ffe1db34
sambutracy <tracysambut@gmail.com>
2024-08-19 15:40:16 +0300

    Add task 1: Learning to use webpack with a config file

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+import debounce from 'lodash/debounce';
+
+// Create and append elements
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button id="start-btn">Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+// Initialize counter
+let count = 0;
+
+// Function to update the counter
+function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+}
+
+// Bind debounce function to button click event
+$('#start-btn').on('click', debounce(updateCounter, 300));
